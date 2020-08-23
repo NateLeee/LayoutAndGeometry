591d82b905b632aea7b026ab9289178bd759c9fc
Nate Lee <nateleee.dev@gmail.com>
2020-08-23 13:12:28 +0800

    - [] ScrollView effects using GeometryReader.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -10,48 +10,34 @@ import SwiftUI
 
 
 
-
-struct OuterView: View {
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        VStack {
-            Text("OuterView Top") // ✅
-            
-            InnerView() // ✅
-                .background(Color.green)
-            
-            Text("OuterView Bottom") // ✅
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("InnerView Left") // ✅
-            
-            GeometryReader { geo in
-                Text("InnerView GeometryReader Center")
-                    .background(Color.blue) // ✅
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0 ..< 50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(
+                                .degrees(
+                                    Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5
+                                ), // ✅
+                                axis: (x: 0, y: 1, z: 0)
+                        )
+                    }
+                    .frame(height: 40)
                 }
             }
-            .background(Color.orange) // ✅
-            
-            Text("InnerView Right") // ✅
         }
     }
 }
 
-struct ContentView: View {
-    var body: some View {
-        OuterView()
-            .background(Color.red) // ✅
-            .coordinateSpace(name: "Custom") // ???
-    }
-}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
